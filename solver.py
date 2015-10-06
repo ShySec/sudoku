c5ca831d2ecf6f5f08cac03eb71c0ed14e2237de
kelson <kelson@shysecurity.com>
2015-10-06 08:26:39 -0400

    added implication analysis

--- solver.py
+++ solver.py
@@ -3,9 +3,12 @@ import sys
 import copy
 import string
 import random
+import logging
 import collections
 
 gPrefix = ''
+
+class Unsolveable(Exception): pass
 
 def read_board(filename, analyze=True, debug=False):
 	layout = []
@@ -39,12 +42,12 @@ def analyze_board(board, debug=False):
 	for row in xrange(width): analyze_board_row(board, row)
 	for col in xrange(width): analyze_board_column(board, col)
 	for square in xrange(width): analyze_board_square(board, square)
+	for square in xrange(width): analyze_board_square_implied(board, square)
 	return board
 
 def analyze_restriction(board, row, column, data):
 	original = board['analysis'].get(row,{}).get(column)
-	if not original: return board
-	original.difference_update(data)
+	if original: original.difference_update(data)
 	return board
 
 def analyze_board_row(board, row):
@@ -60,21 +63,95 @@ def analyze_board_column(board, column):
 	for row in xrange(width): analyze_restriction(board, row, column, data)
 	return data
 
+def get_board_square_entries(board, square):
+	layout,width = board['layout'],len(board['layout'])
+	border = int(width**0.5)
+	roffset = (square/border)*border
+	coffset = (square%border)*border
+
+	data = list()
+	for row in xrange(border):
+		for col in xrange(border):
+			data.append(layout[row+roffset][col+coffset])
+	return data
+
 def analyze_board_square(board, square):
 	layout,width = board['layout'],len(board['layout'])
 	border = int(width**0.5)
-
-	data = set()
 	roffset = (square/border)*border
 	coffset = (square%border)*border
-	for row in xrange(border):
-		for col in xrange(border):
-			data.add(layout[row+roffset][col+coffset])
-	data.discard(0)
+
+	data = set(get_board_square_entries(board, square))
 	for row in xrange(border):
 		for column in xrange(border):
 			analyze_restriction(board, row+roffset, column+coffset, data)
 	return data
+
+def analyze_board_square_implied(board, square, recursive=True):
+	layout,width = board['layout'],len(board['layout'])
+	border = int(width**0.5)
+	roffset = (square/border)*border
+	coffset = (square%border)*border
+
+	analysis = list()
+	for row in xrange(border):
+		for col in xrange(border):
+			analysis.extend(board['analysis'][row+roffset][col+coffset])
+	data = collections.Counter(analysis)
+
+	squares = set()
+	for row in xrange(border): squares.update(analyze_board_square_implied_row(board, square, row, data))
+	for col in xrange(border): squares.update(analyze_board_square_implied_column(board, square, col, data))
+	if recursive:
+		for modified_square in squares: analyze_board_square_implied(board, modified_square)
+	return data
+
+def str_index(row, column):
+	return '%d,%d'%(column+1,row+1)
+
+def analyze_board_square_implied_row(board, square, row, data):
+	layout,width = board['layout'],len(board['layout'])
+	border = int(width**0.5)
+	roffset = (square/border)*border
+	coffset = (square%border)*border
+
+	squares = set()
+	entries = list()
+	for col in xrange(border):
+		entries.extend(board['analysis'][row+roffset][col+coffset])
+	entries = collections.Counter(entries)
+	for entry in entries:
+		if entries[entry] != data[entry]: continue
+		# the values -must- be in this square, not outside on this row
+		for col in xrange(width):
+			if col/border == coffset/border: continue # same square
+			if entry not in board['analysis'][row+roffset][col]: continue
+			logging.debug('analysis eliminating %d from %s by row-implication from square %d',entry,str_index(row+roffset,col),square)
+			analyze_restriction(board, row+roffset, col, [entry]) # eliminate the option
+			squares.add(square_index(board,row+roffset,col)) # record updated square
+	return squares
+
+def analyze_board_square_implied_column(board, square, column, data):
+	layout,width = board['layout'],len(board['layout'])
+	border = int(width**0.5)
+	roffset = (square/border)*border
+	coffset = (square%border)*border
+
+	squares = set()
+	entries = list()
+	for row in xrange(border):
+		entries.extend(board['analysis'][row+roffset][column+coffset])
+	entries = collections.Counter(entries)
+	for entry in entries:
+		if entries[entry] != data[entry]: continue
+		# the values -must- be in this square, not outside on this row
+		for row in xrange(width):
+			if row/border == roffset/border: continue # same square
+			if entry not in board['analysis'][row][column+coffset]: continue
+			logging.debug('analysis eliminating %d from %s by column-implication from square %d',entry,str_index(row,column+coffset),square)
+			analyze_restriction(board, row, column+coffset, [entry]) # eliminate the option
+			squares.add(square_index(board,row,column+coffset)) # record updated square
+	return squares
 
 def bordered(index, border, width):
 	if index == width-1: return False
@@ -125,6 +202,7 @@ def update_board(board, row, column, value, save_state=True):
 	analyze_board_row(board, row)
 	analyze_board_column(board, column)
 	analyze_board_square(board, square_index(board,row,column))
+	analyze_board_square_implied(board, square_index(board,row,column))
 	if save_state: board['states'].append(copy.deepcopy(dict(filter(lambda x:x[0]!='states',board.iteritems()))))
 	return board
 
@@ -138,22 +216,20 @@ def report(data):
 	if gPrefix: print '%s %s'%(gPrefix,data)
 	else: print data
 
-def solve_board(board, save_state=True, debug=False):
+def solve_board(board, brute=True, save_state=True, debug=False):
 	while board['missing']:
-		step = solve_board_step(board, debug=debug)
-		if not step: pass
-		if not step: raise Exception('failed to solve\n%s'%(stringify_board(board)))
+		step = solve_board_step(board, brute=brute, debug=debug)
+		if not step: raise Unsolveable('failed to solve\n%s'%(stringify_board(board)))
 		board = step
 	return board
 
-def solve_board_step(board, debug=False):
+def solve_board_step(board, brute=True, debug=False):
 	update = None
-	apply_update = lambda board,update:update_board(board,update[0],update[1],update[2])
 	if not update: update = solve_board_step_last_remnant(board,debug)
 	if not update: update = solve_board_step_last_in_row(board,debug)
 	if not update: update = solve_board_step_last_in_column(board,debug)
-	if update: return apply_update(board, update)
-	return solve_board_step_random(board,debug)
+	if update: return update_board(board,update[0],update[1],update[2])
+	if brute: return solve_board_step_random(board,debug)
 
 def solve_board_step_last_remnant(board, debug=False):
 	layout,width = board['layout'],len(board['layout'])
@@ -164,7 +240,7 @@ def solve_board_step_last_remnant(board, debug=False):
 			if len(cdata) == 0: raise Exception('unsolveable at %d,%d\n%s'%(row,col,stringify_board(board)))
 			if len(cdata) != 1: continue
 			update = (row,col,cdata.pop())
-			if debug: report('%d,%d => %d (last-remnant)'%update)
+			if debug: report('%d,%d => %d (single-option)'%update)
 			return update
 
 def solve_board_step_last_in_row(board, debug=False):
@@ -222,13 +298,13 @@ def solve_board_step_last_in_square(board, debug=False):
 
 def solve_board_step_random(board, debug=False):
 	layout,width = board['layout'],len(board['layout'])
-	print '\n%s'%stringify_board(board,False)
+	if debug: print '\n%s'%stringify_board(board,False)
 	for row in xrange(width):
 		for col in xrange(width):
 			if layout[row][col] != 0: continue
 			values = sorted(list(board['analysis'][row][col]))
 			for value in values:
-				print 'brute forcing %d,%d=%s => %d'%(row,col,values,value)
+				if debug: print 'brute forcing %d,%d=%s => %d'%(row,col,values,value)
 				global gPrefix
 				gPrefix += '-'
 				bruting_board = copy.deepcopy(board)
@@ -236,10 +312,19 @@ def solve_board_step_random(board, debug=False):
 				try:
 					bruting_board = solve_board(bruting_board, debug=debug)
 				except:
-					print 'brute forcing failed; unwinding'
+					if debug: print 'brute forcing failed; unwinding'
 					bruting_board = None
 				gPrefix = gPrefix[:-1]
 				if bruting_board: return bruting_board
+
+def unittest(path='boards'):
+	import os
+	blacklist=['t04.csv']
+	for test in os.listdir(path):
+		if test in blacklist: continue
+		print test
+		board = read_board(os.path.join(path,test))
+		board = solve_board(board,debug=False)
 
 if __name__ == '__main__':
 	if len(sys.argv)<2:
@@ -248,9 +333,7 @@ if __name__ == '__main__':
 		sys.exit(0)
 	board = read_board(sys.argv[1])
 	print stringify_board(board)
-	#board = update_board(board, 0, 1, 4)
-	#print stringify_board(board)
-	board = solve_board(board,debug=True)
+	board = solve_board(board,brute=True,debug=True)
 	if not board['missing']: print '\npuzzle solved\n'
 	print stringify_board(board)
 	sys.exit(1)
